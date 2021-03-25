7278c4935591f43d0271b65d48c1d69b150d709e
Spaniel Boone <spaniel.boone@gmail.com>
2021-03-25 20:15:37 +1030

    added table of results and styled quiz

--- assets/script.js
+++ assets/script.js
@@ -3,7 +3,16 @@ var quizTime = 60;
 var time = quizTime;
 var quizComplete = false;
 var score = quizTime;
-localStorage.setItem("topScores",JSON.stringify([0,0,0,0,0,0,0,0,0,0]));
+
+function checkLocal(){
+    if(localStorage.getItem("topScores")==null){
+        localStorage.setItem("topScores",JSON.stringify([0,0,0,0,0]));
+        localStorage.setItem("topScoresNames",JSON.stringify(["None","None","None","None","None"]))
+    }
+
+    topScores = JSON.parse(localStorage.getItem("topScores"))  // get top scores
+    topScoresNames = JSON.parse(localStorage.getItem("topScoresNames"))
+}
 
 const radios = document.getElementsByName("radio")  //get radios collection
 
@@ -26,21 +35,33 @@ function toggleDisplayStart(){
     document.getElementById("startQuiz").style.display="none";
     document.getElementById("questions").style.display="block";
     document.getElementById("results").style.display="none";
+    document.getElementById("score").style.display="block";
 }
+
 
 function toggleDisplayComplete(){
     document.getElementById("startQuiz").style.display="none";
     document.getElementById("questions").style.display="none";
     document.getElementById("results").style.display="block";
+    document.getElementById("score").style.display="none";
+
+    document.getElementById("scores-table").style.display = "none"
+    document.getElementById("enter-name").style.display = "block"
+    document.getElementById("restart-div").style.display = "none"
 }
+
 
 function toggleDisplayReset(){
     document.getElementById("startQuiz").style.display="block";
     document.getElementById("questions").style.display="none";
     document.getElementById("results").style.display="none";
+    document.getElementById("score").style.display="none";
 }
 
+
 function startQuiz(){
+    checkLocal();
+
     event.preventDefault();
 
     // toggle the display to the questions div
@@ -49,8 +70,11 @@ function startQuiz(){
     // fill out questions form with Q1
     renderQuestions();
 
+    scoreTimerUpdate();
     startTimer();
+
 }
+
 
 function renderQuestions(){
     //render questions function to show radio boxes.
@@ -64,13 +88,9 @@ function renderQuestions(){
 
     //for loop to render radio button labels
     renderRadioLabels()  // go to next question
+    resetRadio()  //reset all radio options to unchecked
 }
 
-function getRadioLabel(i){
-        var selector = 'label[for=' + radios[i].id + ']';
-        var label = document.querySelector(selector);
-        return label
-}
 
 function renderRadioLabels(){
     for(i=0; i<qOptions.length;i++){
@@ -79,13 +99,35 @@ function renderRadioLabels(){
     }
 }
 
+
+function resetRadio(){
+    for(i=0; i<radios.length;i++){
+        radios[i].checked = false;
+    } 
+}
+
+
+function getRadioLabel(i){
+        var selector = 'label[for=' + radios[i].id + ']';
+        var label = document.querySelector(selector);
+        return label
+}
+
+
 function answerSubmit(){
     event.preventDefault();
+
+    if(hasNotAnswered()){
+        window.alert("please make at least one choice!");
+        return 
+    }
 
     //check answer against marking schema
     checkCorrect = checkAnswer();
 
-    if(checkCorrect == false){window.alert("Eek, that was wrong ...😢")}
+    if(checkCorrect == false){
+        window.alert("Eek, that was wrong ...😢");
+    }
     if(checkCorrect == true){window.alert("Yassss, that was correct! 🥳")}
 
     //update questionNumber
@@ -98,10 +140,21 @@ function answerSubmit(){
     }
 }
 
+
+function hasNotAnswered(){
+    boolRadios = getUserAnswers(radios);
+    allFalse = true  //default to true unless one true is found in the responses
+    for(i=0; i<boolRadios.length; i++){
+        if(boolRadios[i]==true){allFalse = false}
+    }
+    return allFalse
+}
+
+
 function checkAnswer(){
     // all options and choices (checked values in radio buttons) must match
     boolCorrect = questions[questionNumber].answer;
-    boolRadios = getAnswers(radios);
+    boolRadios = getUserAnswers(radios);
     correct = true; //default to true and revert to false if a non-match is found
     for(i=0; i<boolCorrect.length; i++){
         check = boolCorrect[i] == boolRadios[i];
@@ -110,8 +163,9 @@ function checkAnswer(){
     return correct;
 }
 
+
 // returns an array of booleans that indicate which option was selected by the user as their answer
-function getAnswers(radioArray){
+function getUserAnswers(radioArray){
     answers=[];
     for(i=0; i<radioArray.length;i++){
         answers[i] = radioArray[i].checked;
@@ -119,46 +173,11 @@ function getAnswers(radioArray){
     return answers;
 }
 
-function results(){
-    topScores = JSON.parse(localStorage.getItem("topScores"))  // get top scores
-    topScores.sort(function(a, b){return b-a});
-    _index = topScores.indexOf(score)
-    renderResults(_index);
-}
-
-function renderResults(_index){
-    if(_index>=0){
-        renderTopScore()  //function to enter this score in the topScores array in the correct position
-    } else {
-        renderScore()     // renders score with 'not top scores' text
-    }
-}
-
-function renderTopScore(){
-
-}
-
-function renderScore(){
-
-}
-
-function restartQuiz(){
-    resetQuiz()
-}
-
-function resetQuiz(){
-    toggleDisplayReset();
-
-    //reset all quiz variables
-    questionNumber = 0;
-    quizComplete = false;
-    score = quizTime;
-    time = quizTime;
-}
 
 function startTimer(){
     timer = setInterval(quizTimer,1000)
 }
+
 
 function quizTimer(){
     time--;   //update time
@@ -169,23 +188,117 @@ function quizTimer(){
     timerCheck();  //check any completion conditions
 }
 
+
 function scoreTimerUpdate(){
-    document.getElementById("score").innerHTML = "Your score (and time left!) is: " + score;
+    document.getElementById("score").innerHTML = "Your time left is: " + score;
 }
+
 
 function timerCheck(){
 
     if(time<=0){
         clearInterval(timer);  //reset timer
         window.alert("You're out of time!");  //alert user that they have run out of time without completing quiz
-        results();
         toggleDisplayComplete();
     }
 
     if(time > 0 && quizComplete){
         window.alert("You've completed the quiz!")
         clearInterval(timer);
-        results();
         toggleDisplayComplete();
     }
+}
+
+function renderResults(){
+
+    document.getElementById("results-score").innerHTML = score
+
+    renderResultsTable();
+
+    document.getElementById("enter-name").style.display = "none"
+    document.getElementById("scores-table").style.display = "block"
+    document.getElementById("restart-div").style.display = "block"
+}
+
+function renderResultsTable(){
+    playerName=document.getElementById("player-name").value
+    localStorage.setItem("playerName", playerName)
+    minOldScore = Math.min.apply(Math,topScores);
+
+    if(score > minOldScore){
+        newTopScores()
+        _index = topScores.indexOf(score)
+    } else { var _index=99}
+    renderTable(_index)
+    
+    localStorage.setItem("topScores", JSON.stringify(topScores));
+    localStorage.setItem("topScoresNames", JSON.stringify(topScoresNames));
+}
+
+function newTopScores() {
+    topScores.push(score);
+    topScoresNames.push(localStorage.getItem("playerName"))
+
+    SortScores();
+
+    topScores.pop()
+    topScoresNames.pop()
+}
+
+function SortScores(){
+    scoresNew = [...topScores]
+    namesNew = [...topScoresNames]
+    perm = getSortIndex(topScores)
+
+    for(i=0; i<topScores.length; i++){
+        scoresNew[i]=topScores[perm[i]];
+        namesNew[i]=topScoresNames[perm[i]];
+    }
+
+    //reset topScores and topScoresNames
+    topScores = scoresNew
+    topScoresNames = namesNew
+}
+
+function getSortIndex(arr){
+    var result = Array.from(Array(arr.length).keys()).sort((a, b) => arr[a] > arr[b] ? -1 : (arr[b] < arr[a]) | 0)
+    return result
+}
+
+function renderTable(ind){
+
+    for(i=0; i<topScores.length; i++){
+        if(i==ind){
+            tdScore = document.getElementById("score" + (ind+1));
+            tdName = document.getElementById("name" + (ind+1));
+            tdScore.textContent = score;
+            tdName.textContent = playerName;
+            tdScore.style.color = "red";
+            tdName.style.color = "red";
+
+            topScoresNames[i] = playerName;
+            topScores[i] = score;
+        } else {
+            tdScore = document.getElementById("score" + (i+1));
+            tdName = document.getElementById("name" + (i+1));
+            tdScore.textContent = topScores[i];
+            tdName.textContent = topScoresNames[i];
+        }
+    }
+}
+
+function restartQuiz(){
+    event.preventDefault();
+
+    toggleDisplayReset();
+    for(i=0; i<topScores.length; i++){
+        tdScore = document.getElementById("score" + (i+1)).style.color = "black"
+        tdName = document.getElementById("name" + (i+1)).style.color = "black"
+    }
+
+    //reset all quiz variables
+    questionNumber = 0;
+    quizComplete = false;
+    score = quizTime;
+    time = quizTime;
 }
